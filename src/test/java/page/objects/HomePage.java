b288993d407756f16ccdcce5493a0a57113e5c9b
Eniko Zsoldos <enikozsoldos93@gmail.com>
2019-11-21 22:59:49 +0200

    Go to blog page

--- src/test/java/page/objects/HomePage.java
+++ src/test/java/page/objects/HomePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+
 public class HomePage extends BasePage {
     private static final String MENUBAR_ID = "menubar";
     @FindBy(how = How.ID, using = MENUBAR_ID)
@@ -15,6 +16,8 @@ public class HomePage extends BasePage {
     private WebElement headerImage;
     @FindBy(how = How.XPATH, using = "(//a[contains(text(),'Support')])[1]")
     private WebElement supportButton;
+    @FindBy(how = How.XPATH, using = "(//a[contains(text(),'Blog')])[1]")
+    private WebElement blogButton;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver, ExpectedConditions.visibilityOfElementLocated(By.id(MENUBAR_ID)));
@@ -34,5 +37,9 @@ public class HomePage extends BasePage {
         return new SupportPage(driver);
     }
 
+    public BlogPage goToBlogPage() {
+        blogButton.click();
 
+        return new BlogPage(driver);
+    }
 }
--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import page.objects.BlogPage;
 import page.objects.HomePage;
 import page.objects.SupportPage;
 import page.objects.SwitchPage;
@@ -22,5 +23,13 @@ public class HomePageTest extends BaseTest {
 
         Assert.assertEquals(new SupportPage(driver).getLanguage(), "English");
         Assert.assertEquals(new SupportPage(driver).getSubHeaderText(), "Browse by Topic");
+    }
+
+    @Test(description = "Go to blog page")
+    public void goToBlogPage() {
+        new HomePage(driver)
+                .goToBlogPage();
+
+        Assert.assertTrue(new BlogPage(driver).isArticleDisplayed());
     }
 }
